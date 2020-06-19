2dc435030e2e2a75f048f648e1a2a5fdec8a71ba
digitaldilligence <digitaldilligence@gmail.com>
2020-06-19 14:36:44 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todolist';
+  title = 'Todo List';
+  constructor(){
+    this.setPosts();
+    setTimeout(() =>this.getPosts(20), 3000);
+  }
+  ngOnInit(){
+    //console.log(this.posts);
+  }
+  random = Math.random();
+  posts = [];
+
+  setPosts():void{
+    fetch('http://jsonplaceholder.typicode.com/todos').then(data => data.json()).then(res => res.forEach(post =>{
+      this.posts.push(post);
+    }));
+  }
+  getPosts(num: number):void{
+    /**
+     * for all posts with no num argument 
+     * this.posts.forEach(post =>{
+      console.log(post);
+    })
+    Gets first x posts
+    **/
+    for(let i=0; i < num; i++){
+      console.log(this.posts[i].title);
+    }
+  }
+
+  
 }
